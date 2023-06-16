90dbe9f5bcc51e1fcefaa8d5dbd1a8fba6274dd2
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-16 03:02:00 +0300

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
