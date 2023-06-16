d253772cdcc18f5ec68c6250f67e354a188ca626
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-16 03:30:50 +0300

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
