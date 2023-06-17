a2d3b8af05f7c6f23b6bddc19bdda0164debd4cd
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-17 05:50:33 +0300

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(48 + n);
+		putchar(',');
+	}
+	putchar(10);
+	return (0);
+}
