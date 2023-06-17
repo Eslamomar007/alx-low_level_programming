5949f030751a60eb4ba36aaaeaa836b56ae4060d
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-17 19:56:33 +0300

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 57; n++)
+	{
+		for (int i = 49; i <= 57; n++)
+		{
+			putchar(n);
+			putchar(i);
+			if (i == 57)
+			putchar(44);			
+		}
+	}
+	putchar(10);
+	return (0);
+}
