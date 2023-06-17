b3e120b1c8b092f52ca8dd04b243ec75d0051e05
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-17 04:36:57 +0300

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,12 @@
-#include<stdlib.h>
-#include<time.h>
-#include<stdio.h>
-
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
- * * main - main func
- * * description : asdfasdf
- * * return: return 0 
- * */
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
 int main(void)
 {
 	int n;
@@ -14,13 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 	{
-		 printf("%d is zero\n", n);
-        }
+		printf("%d is zero\n", n);
+	}
 	else if (n > 0)
-        {
-	         printf("%d is positive\n", n);
-        }
-        else 
+	{
+		printf("%d is positive\n", n);
+	}
+	else 
 	{
 		printf("%d is negative\n", n);
 	}
