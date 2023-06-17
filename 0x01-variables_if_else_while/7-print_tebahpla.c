7b9ffc80b18bd56847abb53ea9670eb87614eeb6
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-17 05:35:04 +0300

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n <= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
