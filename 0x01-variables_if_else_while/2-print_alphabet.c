e4f94848f7176118e75092352e4bfe6c4ba59c28
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-17 04:56:45 +0300

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = "a"; n <=z; n++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
