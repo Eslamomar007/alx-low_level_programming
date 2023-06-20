981187ddcc68ec95e02eb6c73385b0789a459e3f
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 02:33:37 +0300

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,8 +26,11 @@ void jack_bauer(void)
 		_putchar(hour2 + '0');
 		_putchar(hour1 + '0');
 		_putchar(58);
+		if (minute2 < 10)
+			_putchar(minute1 + '0');
+		
 		_putchar(minute2 + '0');
-		_putchar(minute1 + '0');
+		
 		_putchar('\n');
 		minute2 =+ 1;
 	}
