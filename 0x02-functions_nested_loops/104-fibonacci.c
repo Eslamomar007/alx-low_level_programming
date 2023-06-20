bd4c87299181d70f738378b4ba14913a8506d711
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 01:21:32 +0300

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
-/**
-*      print_last_digit - Prints "Programming is ing new line with puts"
-*      @n: The character to print
-*      Return: Always1
-*/
 
+/**
+ *  * main - prints the alphabet in lowercase, and then in uppercase,
+ *   * followed by a new line
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	int counter = 0;
-	int fi = 1 ;
-	int prev_fi = 1;
-	while (counter < 99)
-	{
-	
-		printf("%d",fi);
-		printf("%d",prev_fi);
-		break;	
+		int ch;
 
-	}
-
+			for (ch = 'a'; ch <= 'z'; ch++)
+						putchar(ch);
+				for (ch = 'A'; ch <= 'Z'; ch++)
+							putchar(ch);
+					putchar('\n');
+						return (0);
 }
-
