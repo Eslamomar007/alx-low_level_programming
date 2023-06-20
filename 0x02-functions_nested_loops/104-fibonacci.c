6f501035b130fd408bb5f862300302ba15274e8c
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 01:00:14 +0300

    done

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*      print_last_digit - Prints "Programming is ing new line with puts"
+*      @n: The character to print
+*      Return: Always1
+*/
+
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+	return (x);
+}
