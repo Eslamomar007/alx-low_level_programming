91f9407f4f211b5ac259b140664350f260aa5469
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 00:16:10 +0300

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*      _abs - Prints "Programming is ing new line with puts"
+*      @n: The character to print
+*      Return: Always1
+*/
+
+int _abs(int n)
+{
+	int x;
+
+	x = abs(x);
+	return (x);
+}
