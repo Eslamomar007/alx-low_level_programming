7a09f752571c8bdd7eb855ab3470e0c9214bf2ad
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-20 15:30:26 +0300

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+void print_alphabet(void)
+{
+	int i = 97;
+	for( i = 97; i <= 122; i++)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+	return (0);
+}
