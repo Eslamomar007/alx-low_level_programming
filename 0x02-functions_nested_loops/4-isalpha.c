ef0871dda36a51a339d1d254383a10e640e97b53
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-20 16:26:00 +0300

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*      _islower - Prints "Programming is ing new line with puts"
+*      @c: The character to print
+*      Return: Always1
+*/
+
+int _islower(int c)
+{
+	if ((c > 96 && c < 123) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
