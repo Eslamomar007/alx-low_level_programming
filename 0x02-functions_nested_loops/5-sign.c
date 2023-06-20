327e9425a1cc24627bc9985036e6abd495241e29
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-20 17:23:53 +0300

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*      _isalpha - Prints "Programming is ing new line with puts"
+*      @c: The character to print
+*      Return: Always1
+*/
+
+int _isalpha(int c)
+{
+	if (n > 0 )
+	{
+		_putchar('+');
+		_putchar(44);
+		_putchar('\n');
+		return (1);
+	}
+  	if (n < 0 )
+        {
+	       	_putchar('-');
+		_putchar(44);
+		_putchar('\n');
+		return ("-1");
+	}
+
+	putchar(48);
+	putchar(44);
+	putchar('\n');
+	return (0);
+}
