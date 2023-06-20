507ffe8e5ddd559a415c078588990c796d196a0c
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 01:28:00 +0300

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the alphabet in lowercase, and then in uppercase,
- *   * followed by a new line
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int ch;
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 
-			for (ch = 'a'; ch <= 'z'; ch++)
-						putchar(ch);
-				for (ch = 'A'; ch <= 'Z'; ch++)
-							putchar(ch);
-					putchar('\n');
-						return (0);
+void main(void)
+{
+	int fi = 2;
+	int prev_fi = 1; 
+	int counter = 1;
+
+	while (counter < 99)
+	{
+		printf("%d, ", fi);
+		fi = fi + prev_fi;
+		counter++;
+	}
+
 }
