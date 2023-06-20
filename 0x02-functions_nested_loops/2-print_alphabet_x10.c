c05e7e6744f4f435afdcd75d1ed4328ac9d606e5
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-20 15:56:43 +0300

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*      print_alphabet - Prints "Programming is li building new line with puts"
+*      print_alphabet_x10 - Prints "Programming is li building new line with puts"
 *
 *      Return: Always1
 */
@@ -10,7 +10,8 @@ void print_alphabet_x10(void)
 {
 	int i = 97;
 	int n = 10;
-	for ( n = 0; n < 10; n++)
+
+	for (n = 0; n < 10; n++)
 	{
 		for (i = 97; i <= 122; i++)
 		{
