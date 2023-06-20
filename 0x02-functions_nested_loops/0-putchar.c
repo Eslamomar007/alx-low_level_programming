184c90f164b6acc1a8f10db9beecf4ca7d806db7
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-20 14:31:50 +0300

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*      main - Prints "Programming is like building new line with puts"
+*
+*      Return: Always1
+*/
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+	return (0);
+}
