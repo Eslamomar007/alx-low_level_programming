f2c83027fd97d37a60bed31914a8ff11267eac3d
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 05:11:12 +0300

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,16 +8,18 @@
 
 int _islower(int c)
 {
-	for(int i = 0; i <= 9; i++)
+	int i; 
+	int x; 
+	for(i = 0; i <= 9; i++)
 	{
-		for(int x = 0; x <= 9; x++)
+		for(x = 0; x <= 9; x++)
 		{ 
 			int input = x + i;
 			_putchar(input);
-			_putchar("44");
-			_putchar("32");
+			_putchar(44);
+			_putchar(32);
 		}
-		_putchar("36");
+		_putchar(36);
 		_putchar('\n');
 	}
 
