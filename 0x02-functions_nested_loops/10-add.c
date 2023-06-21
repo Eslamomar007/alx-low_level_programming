a26d602c58c5bf704b79537e785239aeea1fc792
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 06:00:04 +0300

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add - Adds two valueasdfs
+ * @n1: The first number to asdfbe computed
+ * @n2: The second numbeasdfr to be computed
+ *
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+{
+		return (n1 + n2);
+}
