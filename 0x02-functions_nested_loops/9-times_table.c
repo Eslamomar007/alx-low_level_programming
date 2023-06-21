2342d003c15a0048a0b209a5d04aec7e3f81cdb7
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 14:55:14 +0300

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*      	times_table - prints"Programming is ing new line with puts"
-*
-*	Return: Always1
+* times_table - prints"Programming is ing new line with puts"
+* Return: Always1
 */
 
 void times_table(void)
@@ -15,34 +14,36 @@ void times_table(void)
 	int sec;
 
 	for(x = 0; x <= 9; x++)
+	{
+		for ( i = 0; i <= 9 ; i++)
 		{
-			for ( i = 0; i <= 9 ; i++)
-			{
-				input = i * x;
+			input = i * x;
 			if (input > 9)
 			{
 				sec = input % 10;
 				f = (input - sec) / 10;
- 				_putchar(f + '0');
-			       _putchar(sec + '0');
-			     	 if (i != 9 )
-				 {
-					 _putchar(44);
-			      		 _putchar(32);
-				 }
+				_putchar(f + '0');
+ 				_putchar(sec + '0');
+				if (i != 9 )
+				{
+					_putchar(44);
+					_putchar(32);
+				}
 			}
 			else
 			{
-			if(i != 0)
-				_putchar(' ');	
-			_putchar(input + '0');
-			if( i != 9)
-			{
-				_putchar(44);
-				_putchar(32);
-			}
+				if(i != 0)
+				{
+					_putchar(' ');	
+				}
+				_putchar(input + '0');
+				if( i != 9)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
 			}
 		}
 		_putchar('\n');
-		}
+	}
 }
