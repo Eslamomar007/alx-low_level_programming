48f82fd862bc8ca118f5196616f75105f2ae6772
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 04:45:00 +0300

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,8 +13,10 @@ void jack_bauer(void)
 	int minute2 = 0;
 	int hour1 = 0;
 	int hour2 = 0;
-	
-
+	for (hour1 = 0 ; hour1 <=2; hour1++)
+	{
+ 	for (hour2 = 0; hour2 <= 9; hour2++)
+	{
 	for (minute1 = 0; minute1 <=5; minute1++)
 	{
 	for (minute2 = 0 ; minute2 <= 9; minute2++) 
@@ -27,6 +29,13 @@ void jack_bauer(void)
 		_putchar(minute2 + '0');
 		_putchar('\n');
 	             
-	}}
-
+	}
+	}
+	
+	if (hour2 == 3 && hour1 ==2)
+		break;
+	}
+	if (hour2 == 3 && hour1 == 2)
+		break;
+	}
 }
