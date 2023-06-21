8e4fef47a69a459bb78f1c791662a01c3a287e34
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 22:46:19 +0300

    done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*      print_to_98 - Prints "Programming is ing new line with puts"
+*      @n: The character to print
+*      Return: Always1
+*/
+
+int print_to_98(int n)
+{
+	int i = 98;
+
+	if (n > 98)
+	{
+		for (n ; n <= 98; n--)
+		{
+			_putchar(n + '0');
+			if (n != i)
+			{
+				_putchar(44);
+				_putchar(' ');
+			}
+		}
+	}
+	if (n < 98)
+	{
+		for (n; n < 98; n++)
+		{
+			_putchar(n + '0');
+			if (n != i)
+			{
+				_putchar(44);
+				_putchar(' ');
+			}							
+		}
+	}
+}
