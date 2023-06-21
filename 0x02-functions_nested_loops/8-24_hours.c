5e22b8f5ff8788b7b7ae034497f854316c29876b
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 04:50:37 +0300

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
 *     jack_bauer - Prints "Programming is ing new line with puts"
-*      
+*
 *      Return: Always1
 */
 
@@ -13,15 +13,15 @@ void jack_bauer(void)
 	int minute2 = 0;
 	int hour1 = 0;
 	int hour2 = 0;
-	for (hour1 = 0 ; hour1 <=2; hour1++)
+	
+	for (hour1 = 0 ; hour1 <= 2; hour1++)
 	{
 		for (hour2 = 0; hour2 <= 9; hour2++)
 		{
-			for (minute1 = 0; minute1 <=5; minute1++)
+			for (minute1 = 0; minute1 <= 5; minute1++)
 			{
 				for (minute2 = 0 ; minute2 <= 9; minute2++) 
 				{
-		        
 					_putchar(hour1 + '0');
 					_putchar(hour2 + '0');
 					_putchar(58);
@@ -31,7 +31,7 @@ void jack_bauer(void)
 				}
 			}
 	
-			if (hour2 == 3 && hour1 ==2)
+			if (hour2 == 3 && hour1 == 2)
 				break;
 		}
 		if (hour2 == 3 && hour1 == 2)
