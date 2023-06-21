2e0ea0d702f49a8fc3f5b4aa4c3a86211b30fc4f
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 05:09:07 +0300

    done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*      _islower - Prints "Programming is ing new line with puts"
+*      @c: The character to print
+*      Return: Always1
+*/
+
+int _islower(int c)
+{
+	for(int i = 0; i <= 9; i++)
+	{
+		for(int x = 0; x <= 9; x++)
+		{ 
+			int input = x + i;
+			_putchar(input);
+			_putchar("44");
+			_putchar("32");
+		}
+		_putchar("36");
+		_putchar('\n');
+	}
+
+
+
+
+
+
+}
