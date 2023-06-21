1e54511f6114b28c22f62d5740a1f1be65352503
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 05:38:11 +0300

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,15 +11,35 @@ void times_table(void)
 	int x;
 	int i;
 	int input;
+	int f;
+	int sec;
 
 	for(x = 0; x <= 9; x++)
 		{
 			for ( i = 0; i <= 9 ; i++)
 			{	
+		
+			
+			
 			input = i * x;
+			if (input > 9)
+			{
+				sec = input % 10;
+
+				f = (sec - input)/10;
+	                       _putchar(f + '0');
+			       _putchar(sec + '0');
+			       _putchar(44);
+			       _putchar(32);
+			}
+
+			else
+			{
+		
 			_putchar(input + '0');
 			_putchar(44);
 			_putchar(32);
+			}
 		}
 		_putchar('\n');
 		}
