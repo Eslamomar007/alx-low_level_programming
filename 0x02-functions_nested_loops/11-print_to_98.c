10597a31b2de25624d51886e5f200d515d925373
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 22:53:07 +0300

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (n ; n <= 98; n--)
+		for (n = n ; n <= 98; n--)
 		{
 			_putchar(n + '0');
 			if (n != i)
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	}
 	if (n < 98)
 	{
-		for (n; n < 98; n++)
+		for (n = n ; n < 98; n++)
 		{
 			_putchar(n + '0');
 			if (n != i)
