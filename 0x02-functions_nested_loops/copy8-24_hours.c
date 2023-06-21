c5d9972c4ab01a2bb787f0f88062ebe508632543
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 04:38:30 +0300

    done

---
+++ 0x02-functions_nested_loops/copy8-24_hours.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*      print_last_digit - Prints "Programming is ing new line with puts"
+*      t
+*      Return: Always1
+*/
+
+void jack_bauer(void)
+{
+	int minute1 = 0;
+	int minute2 = 0;
+	int hour1 = 0;
+	int hour2 = 0;
+	int counter = 0;
+
+	while (1)
+	{
+		if (hour2 > 9)
+		{
+			hour2 = 0;
+			hour1 = hour1 + 1;
+		}
+		if (minute1 == 5 && minute2 == 10)
+		{
+			minute1 = 0;
+			minute2 = 0;
+			hour2 = hour2 + 1;
+		
+		}
+		if (minute2 > 9)
+		{
+			minute2 = 0;
+			minute1 = minute1 + 1;
+		}
+                
+		_putchar(hour1 + '0');
+		_putchar(hour2 + '0');
+		_putchar(58);
+		_putchar(minute1 + '0');
+		_putchar(minute2 + '0');
+		_putchar('\n');
+		minute2 = minute2 + 1;
+                if (counter == 1)
+			break;
+		if (hour1 == 2 && hour2 == 3 && minute1 == 5 && minute2 == 9)
+			counter++;
+	                        
+	}
+
+}
