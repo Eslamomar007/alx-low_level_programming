cb67f4ec687cf1670218e5f99f740f0156667e86
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-21 05:29:17 +0300

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,21 @@
 
 void times_table(void)
 {
-	int i;
 	int x;
+	int i;
+	int input;
+
 	for(x = 0; x <= 9; x++)
 		{
-			int input = x + i;
+			for ( i = 0; i <= 9 ; i++)
+			{	
+			input = line * column;
 			_putchar(input + '0');
 			_putchar(44);
 			_putchar(32);
 		}
+		_putchar('\n');
+		}
+	
 
 }
