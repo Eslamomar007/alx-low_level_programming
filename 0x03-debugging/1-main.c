db0897422f495b411102720ff17ffec98994db1a
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-23 02:28:57 +0300

    done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
+ * main - causes an asdfsdafinfinite loop
  * Return: 0
  */
 
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");	
-	
+	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-	
+
 	/*while (i < 10)*/
 	/*{*/
-	/*putchar(i);*/
+	/*	putchar(i);*/
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
