df04ded96422603a50ca71d954f3d9c567de587f
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-22 03:35:20 +0300

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,48 +12,45 @@ void print_times_table(int n)
 	int i;
 	int input;
 
-	for (x = 0; x <= n; x++)
-	{
-		for (i = 0; i <= n ; i++)
+	if (n < 15 || n > 0)
+	{	
+		for (x = 0; x <= n; x++)
 		{
-			input = i * x;
-			if (input > 99)
+			for (i = 0; i <= n ; i++)
 			{
-                                if (i <= n)
-					printf(" ");
-				
-				printf("%d", input);
-				if (i != n)
+				input = i * x;
+				if (input > 99)
 				{
-					printf(",");
-
+                                	if (i <= n)
+						printf(" ");
+					printf("%d", input);
+					if (i != n)
+					{
+						printf(",");
+					}
 				}
-			}
-			else if	(input > 9)
-			{ 
-				if (i <= n)
-					printf("  ");
-				printf("%d", input);		
-				if (i != n)
-				{
-					printf(",");
+				else if	(input > 9)
+				{ 
+					if (i <= n)
+						printf("  ");
+					printf("%d", input);		
+					if (i != n)
+					{
+						printf(",");
+					}
 				}
-			}
-			else
-			{
-				
-				if (i != 0)
-				{
-					printf("   ");
-				}
-				printf("%d", input);
-				if (i != n)
-				{
-					printf(",");
-				
+				else
+				{	
+					if (i != 0)
+						printf("   ");
+			
+					printf("%d", input);
+					if (i != n)		
+						printf(",");
 				}
 			}
 		}
 		printf("\n");
 	}
+}
 }
