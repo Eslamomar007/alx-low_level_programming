867e1c163d6daafa467850581de1fc4196ce71be
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-24 01:43:54 +0300

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - functionsdf
+ * print_numbers- function
  *
- * Description: function asdffasdthat can check uppercase char
- *
- * Return: always 0
-*/
+ * Description: function that can print numbers from 0 to 9
+ */
+
 void print_numbers(void)
 {
 	int i;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
 	}
