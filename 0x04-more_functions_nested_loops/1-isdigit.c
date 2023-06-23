7bcd74395426f3af77314cece676891f07e6462b
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-24 01:16:02 +0300

    done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - functionsdf
+ *
+ * Description: function asdffasdthat can check uppercase char
+ *
+ * @c: input paramasdf
+ *
+ * Return: 1 for true 0 for false
+ */
+
+int _isdigitr(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+}
