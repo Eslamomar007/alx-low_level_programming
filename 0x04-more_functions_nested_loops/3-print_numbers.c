5d2cc2364110edd28030e5121819d729fb696ed1
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-24 01:31:34 +0300

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,7 +6,7 @@
  *
  * Description: function asdffasdthat can check uppercase char
  *
- * Return: 1 for true 0 for false
+ * Return: always 0
  */
 
 void print_numbers(void)
@@ -14,8 +14,7 @@ void print_numbers(void)
 	int i;
 	
 	for (i = 48; i <= 57; i++)
-		_putchar(i + '0');
+		_putchar(i + '0')
+			;
 	_putchar('\n');
-
-	return (0);
 }
