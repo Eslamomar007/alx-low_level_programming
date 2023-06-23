0e8866d0ef36a8520a37eb373bf61930efb36dc0
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-24 01:19:34 +0300

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - functionsdf
+ *
+ * Description: function asdffasdthat can check uppercase char
+ *
+ * @a: input adsf
+ * @c: input paramasdf
+ *
+ * Return: 1 for true 0 for false
+ */
+
+int mul(int a,int c)
+{
+		return (a * c);
+}
