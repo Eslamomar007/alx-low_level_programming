18be37288e56e65828c1790808193c2912f5a503
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-24 05:41:09 +0300

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal- function
+ *
+ * @n: int apram
+ *
+ * Description: function that can print numbers from 0 to 9
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int x;
+
+	if (n >= 0)
+	{
+		for (; x < n; x++)
+		{
+			for (i = 0; i < n; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	else
+	{
+		_putchar('\n');
+	}
+}
