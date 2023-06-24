13a3fd5f56a9cd2fce1d362067fff6edafbe97d4
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-24 05:54:17 +0300

    done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square- function
+ *
+ * @size: int apram
+ *
+ * Description: function that can print numbers from 0 to 9
+ */
+
+void print_square(int size)
+{
+	int i;
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
