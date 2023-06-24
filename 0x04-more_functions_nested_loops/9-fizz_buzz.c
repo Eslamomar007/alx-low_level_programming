43d63f705434328b69159680c28949d8dcea1d71
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-24 12:23:51 +0300

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz- function
+ *
+ * Description: function that can print numbers from 0 to 9
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+			printf("%d ", i)
+	}
+
+	_putchar('\n');
+}
