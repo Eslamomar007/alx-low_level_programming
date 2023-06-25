f0ec3d1fa613286aa8b84ef8b291988c14885adb
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-25 03:52:38 +0300

    done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle- function
+ *
+ * @size: int apram
+ *
+ * Description: function that can print numbers from 0 to 9
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int x;
+	int cointer = size - 1;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (counter >= i) 
+					_putchar(35);
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
