feac94d996b8543a6db4f034d30a03edf29f825d
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-27 00:00:02 +0300

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - update the value
+ * the pointer points to to 98
+ *
+ *  @a: pointer to he variable
+ *  
+ *  return - lenth
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+
+	return(len);
+}
