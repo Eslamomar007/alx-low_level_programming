c325f85b839cf5989e8e1bf5aafb40b2b14bdd3a
Eslamomar007 <eslamoomar007@gmail.com>
2023-06-27 00:26:24 +0300

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - update the value
+ * the pointer points to to 98
+ *
+ *  @s: pointer to he variable
+ */
+
+void print_rev(char *s)
+{
+	char str;
+
+	str = strrev(s);
+	puts(str);
+	puts('\n');
+}
