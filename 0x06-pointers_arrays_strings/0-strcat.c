d1e3793e7723c47329d5dcb49740330f7ac1481d
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-01 00:19:34 +0300

    done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * *_strcat - functionsdf
+ *
+ * Description: function asdffasdthat can check uppercase char
+ *
+ * @dest: iasdf
+ * @src: input paramasdf
+ *
+ * Return: *dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(*dest," ");
+	strcat(*dest,*src);
+	return (*dust);
+}
