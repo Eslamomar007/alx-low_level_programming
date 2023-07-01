aa3f144f23f01dbfd67b1b2eb3d5f03ae6dbc0c3
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-01 04:14:42 +0300

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncpy - functionsdf
+ *
+ * Description: function asdffasdthat can check uppercase char
+ *
+ * @dest: iasdf
+ * @src: input paramasdf
+ * @n: intasdf
+ * Return: *dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src);
+	return (dest);
+}
