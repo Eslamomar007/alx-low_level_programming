f3428bfe71cc65742580d80c32f8d77cd26b0d5b
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-01 05:20:45 +0300

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,26 +2,22 @@
 #include <string.h>
 
 /**
- * _strcmp - functionsdf
- *
+ * _strcmp - functionsd
  * Description: function asdffasdthat can check uppercase char
- *
  * @dest: iasdf
  * @src: input paramasdf
- *
- * Return: 0
+ * Return: int a 0
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 
-	for(a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
 	{
-		if(s1[a] != s2[a])
+		if (s1[a] != s2[a])
 		{
 			return (s1[a] - s2[a]);
 		}
 	}
-	return(0);
+	return (0);
 }
