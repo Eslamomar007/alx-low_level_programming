20fa9c668ca9e135c594a40eb771181cdc631c69
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-01 04:09:35 +0300

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
