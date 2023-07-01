3a9b23975bc4efddbd7e3395f78a3995bd4770e9
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-01 05:48:43 +0300

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *string_toupper(char *);
 char *_strncpy(char *dest, char *src, int n);
 int _putchar(char c);
 int _isupper(int c);
