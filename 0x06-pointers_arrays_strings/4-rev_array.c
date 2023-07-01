d9f6cd9477f420cb5fd168a291cb4f4115abc17d
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-01 05:29:16 +0300

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - functionsd
+ * Description: function asdffasdthat can check uppercase char
+ * @a: iasdf
+ * @n: input paramasdf
+ * Return: int a 0
+ */
+void reverse_array(int *a, int n)
+{
+	int c = 0;
+	int temp;
+
+	while (c < (n / 2))
+	{
+		temp = a[c];
+		a[c] = a[n-1];
+		a[n-1] = temp;
+	}
+}
