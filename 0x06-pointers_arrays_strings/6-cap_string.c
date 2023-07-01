555abf447a1e26aba606ce59829e8262ff06590a
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-01 12:29:32 +0300

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,14 +9,30 @@
  */
 char *cap_string(char *strs)
 {
-	int b;
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (b = 0; strs[b] != '\0'; b++)
+	i = 0;
+	while (*(strs + i) != '\0')
 	{
-		if (strs[b] >= 'A' && strs[b] <= 'Z')
+		if (*(strs + i) >= 'a' && *(strs + i) <= 'z')
 		{
-			strs[b] = strs[b] + 32;
+			if (i == 0)
+			{
+				*(strs + i) = *(strs + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(strs + i - 1))
+					{
+						*(strs + i) = *(strs + i) - 32;
+					}
+				}
+			}
 		}
+		i++;
 	}
 	return (strs);
 }
