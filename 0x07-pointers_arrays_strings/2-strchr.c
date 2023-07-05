4a384bcc37712bbb55bb278209ce969d9d80ab77
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-05 03:29:18 +0300

    done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - update the valueasdf
+ *
+ *  @s: pointer to he variable
+ *  @c: asdfasdff
+ *
+ *  Return:  lenth
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == c))
+		{
+			return (i);
+		}
+	}
+
+	return (NULL);
+}
