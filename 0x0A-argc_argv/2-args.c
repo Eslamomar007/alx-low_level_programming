8d5b1a3bb236f383bee99d0a5f8b7dc8e8952326
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-08 02:44:36 +0300

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+
+	return (0);
+}
