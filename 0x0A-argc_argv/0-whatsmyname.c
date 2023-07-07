65f19b013ddf3aa5b4d8c1f2d0ec206ec7d3a53f
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-07 22:30:44 +0300

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * main - update the value
+ * the pointer points to to 98
+ *
+ *  @argc: pointer to he variable
+ *  @argv:  asdf
+ *  return 
+ */
+
+int main(int argc, char **argv)
+{
+	printf(argv[0]);
+	return (0);
+}
