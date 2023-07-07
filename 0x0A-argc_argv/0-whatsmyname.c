d79ba936345f684ecbeac7fb689d9b4fc1649753
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-08 02:20:09 +0300

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
  *
  *  @argc: pointer to he variable
  *  @argv:  asdf
- *  return 
+ *  return: asdf 0
  */
 
 int main(int argc, char **argv)
 {
-	printf("\s\n",argv[0]);
+	printf("%s\n",argv[0]);
 	return (0);
 }
