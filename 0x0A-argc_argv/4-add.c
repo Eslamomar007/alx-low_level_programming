3fbf8bed7e85d00b9859156d57522e5104c49fd5
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-08 03:00:42 +0300

    done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, results = 0;
+
+	while (*argv)
+	{
+		if (isdigit(*argv))
+		{
+			results =+ *argv;
+			argv = argv + 1;
+		}
+		else
+		{
+			printf("%s\n", "Error");
+			return (1);
+	}
+        printf("%d\n", results);
+	return (0);
+
+}
