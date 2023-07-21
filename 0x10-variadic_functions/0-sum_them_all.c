529df49fd4dd1c98c0f95528e307f99c2201348a
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-21 05:29:11 +0300

    done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - update the valueoints to to 98
+ *
+ *  @n: pointer to he variable
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	int i;
+
+	va_start(args, n);
+	for (; i < n; i++)
+		sum = va_start(args, int);
+
+	return sum;
+}
---
+++ 0x10-variadic_functions/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef SUM_H
+#define SUM_H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
