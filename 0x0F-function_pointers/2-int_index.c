14f7a01b4cdbc76589e6a56bbea1eb6d4cc6ebf0
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-21 03:57:24 +0300

    done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - asdfdsar
+ * @array: char string
+ * @size: asdfadf
+ * @cmp: iion pointer that takes a string argument
+ * return : afasdfasdfa
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if (size >= 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i])
+					return (i);
+		}
+	}
+	return (-1);
+}
