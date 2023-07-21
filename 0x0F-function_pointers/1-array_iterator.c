867c05f1d6804e5b8a7c2b578750f21cd4ad4bc1
Eslamomar007 <eslamoomar007@gmail.com>
2023-07-21 03:36:01 +0300

    done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - asdfdsar
+ * @array: char string
+ * @size: asdfadf
+ * @size: iion pointer that takes a string argument
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int size_i;
+	
+	for (; i < size; i++)
+		action(array[i]);
+}
