c55978b32acc6124e452b431fb05897919c506e2
Eslamomar007 <eslamoomar007@gmail.com>
2023-11-03 22:32:05 +0200

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -28,9 +28,9 @@ int checker(int base, int f)
 {
 	if (f == base  * base)
 		return (base);
-	if (f > base * base)
-		return (-1);
 	if (f < base * base)
+		return (-1);
+	if (f > base * base)
 		return (checker(base + 1, f));
-	return 1;
+	return (1);
 }
