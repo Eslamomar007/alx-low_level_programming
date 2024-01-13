7f3176bbb0ef8e9adbe851fc4c68f9121d6f72dc
Eslamomar007 <eslamoomar007@gmail.com>
2024-01-13 16:12:34 +0200

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - a program that prints its name, followed by a new line
  * @argc: arguement count
  * @argv: argument value, a strings that come after calling function
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	int i, coins;
@@ -43,6 +45,8 @@ int main(int argc, char *argv[])
 	}
 	if (i == 1)
 		sum++;
+	
 	printf("%d\n", sum);
+
 	return (0);
 }
