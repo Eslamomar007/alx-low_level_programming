d992c96a674411776aaaf3708cb75d6e1a38fec9
Eslamomar007 <eslamoomar007@gmail.com>
2024-01-18 22:50:20 +0200

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,14 +13,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int count, count1;
 	int sign = n;
-	char *ptr1;
+	char *ptr;
 	int len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
+
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
@@ -29,20 +29,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (sign >= len2)
 	{
 		sign = len2;
-		ptr1 = malloc(sizeof(char) * (len1 + len2 + 1));
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	}
 	else
-		ptr1 = malloc(sizeof(char) * (len1 + n + 1));
-	if (ptr1 == NULL)
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
 		return (NULL);
 	for (count = 0; count < len1; count++)
 	{
-		ptr1[count] = s1[count];
+		ptr[count] = s1[count];
 	}
 	for (count1 = 0; count1 < sign; count1++)
 	{
-		ptr1[count++] = s2[count1];
+		ptr[count++] = s2[count1];
 	}
-	ptr1[count++] = '\0';
-	return (ptr1);
+	ptr[count++] = '\0';
+	return (ptr);
 }
