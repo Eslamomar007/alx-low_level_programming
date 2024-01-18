cb68ff4505f5041c768e53ee37411c70fa646d24
Eslamomar007 <eslamoomar007@gmail.com>
2024-01-18 23:23:20 +0200

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	if (size == 0)
 		return (NULL);
-	ptr = maloc(nmemb * size);
+	ptr = malloc(nmemb * size);
 	
 	if (ptr == NULL)
 		return (NULL);
