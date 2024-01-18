b87cbeb9bf753f5cb9ee8fa9b3ece4591599050b
Eslamomar007 <eslamoomar007@gmail.com>
2024-01-18 22:34:21 +0200

    done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 
 void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 char *_memset(char *s, char b, unsigned int n);
