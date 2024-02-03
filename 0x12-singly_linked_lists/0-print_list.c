6618281f170727f8abf77382523639606f5b8cb7
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-03 15:25:29 +0200

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,13 @@ size_t print_list(const list_t *h)
 	{
 			if (h->str = NULL )
 			{
-				printf("[0] (nil)");
+				printf("[0] (nil)\n");
 				counter++;
 				continue;
 			}
 			else
 			{
-				printf("[%d] %s", h->len, h->str);
+				printf("[%d] %s\n", h->len, h->str);
 				counter++;
 			}
 			h = h->next;
