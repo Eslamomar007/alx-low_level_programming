a0d3795885fcb16824af87198881948105435b18
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-04 10:23:07 +0200

    done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,9 @@
 #include "lists.h"
-
 /**
- * add_node - hello
- * @head: list of node
- * @str: string 
- * Return: number of nodes
+ * add_node - add a new node at the end of `list_t` list
+ * @head: double pointer to head
+ * @str: string to duplicate into new node
+ * Return: Address of the new element or NULL if failed
  */
 
 list_t *add_node(list_t **head, const char *str)
