6b77628a2e54ec02c5e44a349ae356a7e5656720
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-04 10:26:52 +0200

    done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,19 +10,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t n = 0;
-
 	new = malloc(sizeof(list_t));
-	
+
 	if (!(new))
 		return (NULL);
-
 	new->str = strdup(str);
-
 	while (str[n])
 		n++;
 	new->len = n;
 	new->next = *head;
 	*head = new;
-
 	return (*head);
 }
