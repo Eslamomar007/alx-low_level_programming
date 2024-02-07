0a00e32e314da478c16cbd7eb39666cd8defa186
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-07 17:11:17 +0200

    done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - add a new node at the end of `list_t` list
+ * @head: double pointer to head
+ * Return: Address of the new element or NULL if failed
+ */
+
+void free_listint2(listint_t **head)
+{
+	lsitint_t *temp, *cur;
+
+	if (head == NULL)
+		return;
+	cur = *head;
+	while (cur != NULL)
+	{
+		temp = cur;
+		cur = cur->next;
+		free(tmep);
+	}
+	*head = NULL;
+}
