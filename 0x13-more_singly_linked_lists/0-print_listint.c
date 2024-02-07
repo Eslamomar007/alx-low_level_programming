bf9257ac3d7012db912c4534dcac32bfdcd72b22
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-07 11:15:10 +0200

    done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_listint - add a new node at the end of `list_t` list
+ * @h: double pointer to head
+ * Return: Address of the new element or NULL if failed
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n;
+	
+	n = 0;
+	if (h)
+		return (-1);
+	while (!(h))
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
