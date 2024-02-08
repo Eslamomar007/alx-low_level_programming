4ea348b4a72237ebe182fb2d28cbac20eca52c62
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-08 11:04:15 +0200

    done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -46,7 +46,7 @@ list_t *add_node_end(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 void free_list(list_t *head);
 
 #endif
