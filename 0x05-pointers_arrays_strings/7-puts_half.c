52fce1788952c390fc4559f57f368f9542879315
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-15 11:50:02 +0200

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - update the value
+ * the pointer points to to 98
+ *
+ * @s: pointer to he variable
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int mid;
+	char temp;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	if (mid % 2 != 0)
+	{
+		mid =(i - 1) / 2;
+	}
+	else
+		mid = i / 2;	
+	while (1)
+	{
+		if (i <= mid)
+		{
+			_putchar(mid);
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+}
