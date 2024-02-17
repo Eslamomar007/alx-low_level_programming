ede5c8331c43b82603c9aa14cc56155c2902c4c9
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-17 11:12:16 +0200

    done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - update the value
+ * the pointer points to to 98
+ *
+ * @dest: asdf
+ * @src: pointer to he variable
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = ('\0');
+	return (dest);
+}
